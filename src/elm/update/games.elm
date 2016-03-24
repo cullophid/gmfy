756fc922cef3483130f1a5f16a743ed84faed8ef
Andreas Møller <andreas.moller@gmail.com>
2016-03-24 07:52:53 +0000

    fixed player score update

--- src/elm/update/games.elm
+++ src/elm/update/games.elm
@@ -1,18 +1,27 @@
 module Update.Games where
 import Actions
+import Util.Map as Map
+import Util.List exposing (last)
+import String
 import Model exposing (Model, Model, Game, GameEntry)
 import Actions exposing (..)
 
 games : Action -> Model -> Model
 games action model =
-  {model | games = updateGames action model.games}
-
-
-updateGames : Action -> List GameEntry -> List GameEntry
-updateGames action games =
-  case action of
-    CreateGame game -> createGameEntry game games
-    _ -> games
+    let
+      {location, games, user} =
+        model
+      gameId =
+        Maybe.withDefault "" <| last <| String.split "/" location
+    in
+      case action of
+        CreateGame game ->
+          {model | games = createGameEntry game games}
+        CompleteTask {value} ->
+          { model
+          | games = Map.update gameId (updatePlayerScore user value) games
+          }
+        _ -> model
 
 
 createGameEntry : Game -> List GameEntry -> List GameEntry
@@ -24,3 +33,13 @@ createGameEntry game gamesList =
 
 nextId games =
   toString <| List.length games
+
+
+updatePlayerScore : String -> Int -> Game -> Game
+updatePlayerScore id value game =
+  let
+    {players} = game
+  in
+    { game
+    | players = Map.update id (\player -> {player | score = value + player.score}) players
+    }
